ea88cc367101c3a692236699b91d1e72c8253c98
Wemerson <wemersonemao@gmail.com>
2024-11-12 09:59:23 -0300

    first commit

---
+++ src/main/java/projetos/wemerson/WemersonApplication.java
@@ -0,0 +1,13 @@
+package projetos.wemerson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WemersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WemersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projetos/wemerson/WemersonApplicationTests.java
@@ -0,0 +1,13 @@
+package projetos.wemerson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WemersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
